020d12cb123f5b34927605faaeee40a68806a21e
Jan Forst <ensonador@gmail.com>
2026-04-06 09:16:54 +0200

    settings + info

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite-plus'
+
+export default defineConfig({
+  // fmt: {},
+  // lint: { options: { typeAware: true, typeCheck: true } },
+  fmt: {
+    semi: false,
+    singleQuote: true,
+  },
+  lint: {
+    plugins: ['eslint', 'typescript', 'unicorn', 'oxc', 'vue', 'vitest'],
+    env: {
+      browser: true,
+    },
+    categories: {
+      correctness: 'error',
+    },
+    options: {
+      typeAware: true,
+    },
+  },
+  run: {
+    cache: true,
+  },
+})
